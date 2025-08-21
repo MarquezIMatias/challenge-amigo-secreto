260a526295ab227fe849da7f1a6b41208b701d60
Matias Marquez <marquezmatiasivan@gmail.com>
2025-08-21 00:48:53 -0300

    Se crea array y funciones

--- app.js
+++ app.js
@@ -2,7 +2,20 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 // Se crea Array para nombres
-
 let amigos = [];
 
+// Se Crea funcion para agregar amigos
+function agregarAmigo() {
+   let amigoSecreto = document.getElementById('amigo').value;
+   console.log(amigoSecreto);
+   limpiarBloque();
+   return;
+}
 
+function generarAmigoSecreto() {
+    
+}
+// Funcion para limpiar bloque
+function limpiarBloque() {
+    document.querySelector('#amigo').value = ''; 
+}
