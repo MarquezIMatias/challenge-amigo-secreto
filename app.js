9f13c6075ad9690cce6bcb073ba51f36479e7f11
Matias Marquez <marquezmatiasivan@gmail.com>
2025-08-22 01:31:40 -0300

    Se modifica js y el index

--- app.js
+++ app.js
@@ -4,17 +4,39 @@
 // Se crea Array para nombres
 let amigos = [];
 
-// Se Crea funcion para agregar amigos
+// Se crea funcion para agregar amigos
 function agregarAmigo() {
-   let amigoSecreto = document.getElementById('amigo').value;
-   console.log(amigoSecreto);
-   limpiarBloque();
-   return;
+   let nombreAmigo = document.getElementById('amigo').value;
+   if (nombreAmigo === '') {
+        alert("Por favor, Ingrese un Nombre Válido"); // Mensaje Alerta nombre
+   } else {
+        amigos.push (nombreAmigo);// Agrega al array un nombre nuevo.
+        // Imprime los nombres en lista.
+        let listadoDeAmigos = document.getElementById('listaAmigos');
+        let contenido = "";
+        for (let amigo of amigos) {
+            contenido += `<li> ${amigo} </li>`;
+            listadoDeAmigos.innerHTML = contenido;
+        }
+        limpiarBloque();
+   } 
 }
 
 function generarAmigoSecreto() {
+    if (amigos == "") {
+        alert("No hay ningún nombre en la Lista de Amigos");// Para saber si no hay amigos en la lista.-
+    } else {
+        let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+        let amigoSecreto = amigos[indiceAleatorio];
+        console.log(indiceAleatorio);
+        console.log(amigoSecreto);
+        let resultadoSorteo = document.getElementById('resultado');
+        resultadoSorteo.innerHTML = `Felicidades "${amigoSecreto}" es tú Amigo Secreto`;
+
+    }
     
 }
+
 // Funcion para limpiar bloque
 function limpiarBloque() {
     document.querySelector('#amigo').value = ''; 
